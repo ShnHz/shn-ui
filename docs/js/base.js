0b3adbd326aadad2c8d78032178c6f4cfe9e83cb
桑杭宁 <sanghangning@corp.netease.com>
2019-04-25 18:58:28 +0800

    init docs

---
+++ docs/js/base.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+
+//格式化代码,去除行前空格
+Vue.prototype.fCode = function (code) {
+  let code_list = []
+  code.trim().split('\n').forEach(function (v) {
+    code_list.push(v)
+  })
+
+  let fcode = ''
+  if (code_list.length > 1) {
+    let length = code_list[code_list.length - 1].match(/(^(?:\s|\t)+)/);
+    length = length[0].length
+    for (let i = 1; i < code_list.length; i++) {
+      code_list[i] = code_list[i].slice(length)
+    }
+    for (let i = 0; i < code_list.length; i++) {
+      fcode = fcode + code_list[i] + '\n'
+    }
+  }else {
+    fcode = code_list[0]
+  }
+
+
+  return fcode
+}
